e352fd5026f4c75b2c4a5ff90944e0adcee7edfa
vannsakal <sakal.suon@gmail.com>
2025-08-14 14:18:16 +0700

    .

--- exercise4_bonus.cpp
+++ exercise4_bonus.cpp
@@ -2,12 +2,39 @@
 using namespace std;
 
 int mat[2][3] = {
-{ 1, 2, 3 },
-{ 4, 5, 6 }
-};
-int mat2[3][2] = {
-{1,2},
-{3,4},
-{5,6}
+    {1, 2, 3},
+    {4, 5, 6}
 };
 
+int mat2[3][2] = {
+    {1, 2},
+    {3, 4},
+    {5, 6}
+};
+
+void multiplyMatrices(int mat1[][3], int mat2[][2], int result[][2], int rows1, int cols1, int cols2) {
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int result[2][2];
+    
+    multiplyMatrices(mat, mat2, result, 2, 3, 2);
+    
+    cout << "Resultant matrix:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
